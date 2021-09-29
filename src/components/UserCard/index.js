ddf4203bb6e0b589639cc189c95d12347d6b5f70
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2021-09-29 09:05:07 -0300

    ajustado o combineReducers e usado o styled-components

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,24 +1,28 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeName } from "../../store/modules/user/actions";
+import { Button, Container, Input } from "../../styles";
 const UserCard = () => {
   const [newName, setNewName] = useState("");
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
   const handleClick = () => {
     dispatch(changeName(newName));
   };
 
   return (
-    <div>
-      <h1>{newName}</h1>
-      <input
-        type="text"
-        value={newName}
-        onChange={(e) => setNewName(e.target.value)}
-      />
-      <button onClick={handleClick}>Change</button>
-    </div>
+    <Container>
+      <h1>User name: {user.name}</h1>
+      <div>
+        <Input
+          type="text"
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+        />
+        <Button onClick={handleClick}>Change</Button>
+      </div>
+    </Container>
   );
 };
 
---
+++ src/styles/index.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+  height: 500px;
+  background-color: #010d26;
+  justify-content center;
+`;
+
+export const Button = styled.button`
+  padding: 5px;
+  background: #637aa6;
+  border: none;
+  margin-left: 10px;
+  width: 150px;
+  border-radius: 10px;
+  color: #ddd;
+  font-size: 1rem;
+`;
+
+export const Input = styled.input`
+  padding: 5px;
+  border-radius: 5px;
+`;
